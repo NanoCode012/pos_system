52eb4323d9a8c2933703961ddcd61c59a14de9be
Passara <passaraink@gmail.com>
2020-11-02 23:45:14 +0700

    did login page and add account
    did login page and add account

---
+++ login.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <title>ChanChan POS System</title>
+      <!-- Favicon -->
+      <link rel="icon" href="assets/img/brand/favicon.png" type="image/png">
+      <!-- Fonts -->
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
+      <!-- Icons -->
+      <link rel="stylesheet" href="assets/vendor/nucleo/css/nucleo.css" type="text/css">
+      <link rel="stylesheet" href="assets/vendor/@fortawesome/fontawesome-free/css/all.min.css" type="text/css">
+      <!-- Argon CSS -->
+      <link rel="stylesheet" href="assets/css/argon.css?v=1.2.0" type="text/css">
+    </head>
+
+    <body class="bg-default">
+      <!-- Main content -->
+      <div class="main-content">
+        <!-- Header -->
+        <div class="header bg-gradient-primary py-7 py-lg-7 pt-lg-6">
+          <div class="container">
+            <div class="header-body text-center mb-7">
+              <div class="row justify-content-center">
+                <div class="col-xl-5 col-lg-6 col-md-2 px-5">
+                  <h1 class="text-white">Welcome to ChanChan's Point of Sale System</h1>
+                  <p class="text-lead text-white">Please enter your username and email to log into the system.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="separator separator-bottom separator-skew zindex-100">
+            <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1" xmlns="http://www.w3.org/2000/svg">
+              <polygon class="fill-default" points="2560 0 2560 100 0 100"></polygon>
+            </svg>
+          </div>
+        </div>
+        <!-- Page content -->
+        <div class="container mt--9 pb-5">
+          <div class="row justify-content-center">
+            <div class="col-lg-5 col-md-7">
+              <div class="card bg-secondary border-0 mb-0">
+                <div class="card-body px-lg-5 py-lg-5">
+                  <div class="text-center text-muted mb-4">
+                    <small>Sign in with username and password</small>
+                  </div>
+                  <form role="form">
+                    <div class="form-group mb-3">
+                      <div class="input-group input-group-merge input-group-alternative">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text"><i class="ni ni-circle-08"></i></span>
+                        </div>
+                        <input class="form-control" placeholder="Username" type="text">
+                      </div>
+                    </div>
+                    <div class="form-group">
+                      <div class="input-group input-group-merge input-group-alternative">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
+                        </div>
+                        <input class="form-control" placeholder="Password" type="password">
+                      </div>
+                    </div>
+                    <div class="text-center">
+                      <button type="button" class="btn btn-primary my-4">Sign in</button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Footer -->
+      <footer class="py-5" id="footer-main">
+        <div class="container">
+          <div class="row align-items-center justify-content-xl-between">
+            <div class="col-xl-6">
+            </div>
+          </div>
+        </div>
+      </footer>
+      <!-- Argon Scripts -->
+      <!-- Core -->
+      <script src="assets/vendor/jquery/dist/jquery.min.js"></script>
+      <script src="assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+      <script src="assets/vendor/js-cookie/js.cookie.js"></script>
+      <script src="assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js"></script>
+      <script src="assets/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js"></script>
+      <!-- Argon JS -->
+      <script src="assets/js/argon.js?v=1.2.0"></script>
+    </body>
+
+</html>
