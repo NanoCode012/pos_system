f64276a4564b393df3149289ed3f226cf04fc84e
NanoCode012 <kevinvong@rocketmail.com>
2020-11-05 19:28:19 +0700

    Fix unknown page redirect issue

--- pages/404.php
+++ pages/404.php
@@ -4,6 +4,6 @@ $redirect = 'index.php';
 if (isset($_SERVER['HTTP_REFERER'])) {
     $redirect = $_SERVER['HTTP_REFERER'];
 }
-echo '<META HTTP-EQUIV="Refresh" Content="0; URL="' . $redirect . '">';
+echo '<META HTTP-EQUIV="Refresh" Content="0; URL=' . $redirect . '">';
 
 ?>
