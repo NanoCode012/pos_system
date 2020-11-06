9cbcc4d228933a1a0eb9ece1f040c361173761a1
NanoCode012 <kevinvong@rocketmail.com>
2020-11-06 18:24:01 +0700

    Remove 404 backup page

--- pages/404.bck.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$redirect = 'index.php';
-if (isset($_SERVER['HTTP_REFERER'])) {
-    $redirect = $_SERVER['HTTP_REFERER'];
-}
-echo '<META HTTP-EQUIV="Refresh" Content="0; URL=' . $redirect . '">';
-
-?>
