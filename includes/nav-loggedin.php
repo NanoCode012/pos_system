a155dc3389a23dea296ff7e5ef9e98a02964ca84
NanoCode012 <kevinvong@rocketmail.com>
2020-11-06 21:24:47 +0700

    Add nav item active when on its page

--- includes/nav-loggedin.php
+++ includes/nav-loggedin.php
@@ -14,31 +14,41 @@
                 <!-- Nav items -->
                 <ul class="navbar-nav nav">
                     <li class="nav-item nav-tab-margin">
-                        <a class="nav-link" href="?p=dashboard">
+                        <a class="nav-link <?php if ($page === 'dashboard') {
+                            echo 'active';
+                        } ?>" href="?p=dashboard">
                             <i class="ni ni-chart-bar-32 text-primary"></i>
                             <span class="nav-link-text">Dashboard</span>
                         </a>
                     </li>
                     <li class="nav-item nav-tab-margin">
-                        <a class="nav-link" href="?p=stocks">
+                        <a class="nav-link <?php if ($page === 'stocks') {
+                            echo 'active';
+                        } ?>" href="?p=stocks">
                             <i class="ni ni-collection" style="color:#a800c2"></i>
                             <span class="nav-link-text">Stocks</span>
                         </a>
                     </li>
                     <li class="nav-item nav-tab-margin">
-                        <a class="nav-link" href="?p=products">
+                        <a class="nav-link <?php if ($page === 'products') {
+                            echo 'active';
+                        } ?>" href="?p=products">
                             <i class="ni ni-planet text-orange"></i>
                             <span class="nav-link-text">Products</span>
                         </a>
                     </li>
                     <li class="nav-item nav-tab-margin">
-                        <a class="nav-link" href="?p=branches">
+                        <a class="nav-link <?php if ($page === 'branches') {
+                            echo 'active';
+                        } ?>" href="?p=branches">
                             <i class="ni ni-pin-3 text-primary"></i>
                             <span class="nav-link-text">Branches</span>
                         </a>
                     </li>
                     <li class="nav-item nav-tab-margin">
-                        <a class="nav-link" href="?p=accounts">
+                        <a class="nav-link <?php if ($page === 'accounts') {
+                            echo 'active';
+                        } ?>" href="?p=accounts">
                             <i class="ni ni-single-02 text-yellow"></i>
                             <span class="nav-link-text">Accounts</span>
                         </a>
