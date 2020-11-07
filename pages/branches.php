6a9035fbbef9abd31a8713be8fb4bac662c71677
Passara <passaraink@gmail.com>
2020-11-07 22:54:05 +0700

    done branches page

--- pages/branches.php
+++ pages/branches.php
@@ -79,6 +79,12 @@
         <div class="col-lg-6 col-7">
           <h6 class="h2 text-white d-inline-block mb-0">Branches</h6>
         </div>
+        <div class="col-lg-6 col-5 text-right">
+            <!-- Button trigger modal -->
+            <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#createBranchModal" style="margin-right:20px; height:45px; border-radius:2em; color:#8898aa;">
+                + New Branch
+            </button>
+        </div>
       </div>
     </div>
   </div>
@@ -118,7 +124,7 @@
                             <i class="fas fa-ellipsis-v"></i>
                             </a>
                             <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
-                            <a class="dropdown-item" data-toggle="modal" data-target="#createBranchModal>Edit branch</a>
+                            <a class="dropdown-item" data-toggle="modal" data-target="#editBranchModal">Edit branch</a>
                             <a class="dropdown-item">Delete branch</a>
                             </div>
                         </div>
@@ -135,12 +141,12 @@
 </div>
 </div>
 
-<!-- Modal create new branch -->
+<!-- Modal create branch -->
 <div class="modal fade" id="createBranchModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-dialog-centered" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Create New Branch</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Create Branch</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
@@ -150,31 +156,51 @@
             <fieldset>
                 <div class="form-group mb-3">
                     <div class="input-group input-group-merge input-group-alternative modal-div-input">
-                        <input class="form-control modal-div-input" placeholder="Product Name" name="name" type="text">
-                    </div>
-                </div>
-                <div class="form-group mb-3">
-                    <select class="form-control create-acc-select modal-div-input" name="category" required>
-                        <option value="" disabled selected> Choose Product Category </option>
-                        <option value="ELECTRONICS"> ELECTRONICS </option>
-                        <option value="DRINKS"> DRINKS </option>
-                        <option value="SNACKS"> SNACKS </option>
-                        <option value="MEDICAL"> MEDICAL </option>
-                    </select>
-                </div>
-                <div class="form-group mb-3">
-                    <div class="input-group input-group-merge input-group-alternative modal-div-input">
-                        <input class="form-control modal-div-input" placeholder="Sale Price" name="sell_price" type="number">
+                        <input class="form-control modal-div-input" placeholder="Branch Name" name="name" type="text">
                     </div>
                 </div>
                 <div class="form-group mb-3">
                     <div class="input-group input-group-merge input-group-alternative modal-div-input">
-                        <input class="form-control modal-div-input" placeholder="Buy Price" name="buy_price" type="number">
+                        <input class="form-control modal-div-input" placeholder="Address" name="sell_price" type="number">
                     </div>
                 </div>
                 <div class="modal-footer">
                   <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
                   <button name="create" type="submit" class="btn btn-primary">Create</button>
+                </div>
+            </fieldset>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Modal edit branch -->
+<div class="modal fade" id="editBranchModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Edit Branch</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+         <form role="form" action="" method="post">
+            <fieldset>
+                <div class="form-group mb-3">
+                    <div class="input-group input-group-merge input-group-alternative modal-div-input">
+                        <input class="form-control modal-div-input" placeholder="Branch Name" name="name" type="text">
+                    </div>
+                </div>
+                <div class="form-group mb-3">
+                    <div class="input-group input-group-merge input-group-alternative modal-div-input">
+                        <input class="form-control modal-div-input" placeholder="Address" name="sell_price" type="number">
+                    </div>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                  <button name="create" type="submit" class="btn btn-primary">Save changes</button>
                 </div>
             </fieldset>
         </form>
