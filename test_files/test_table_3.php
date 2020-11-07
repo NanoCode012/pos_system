ea49387e3e5ee2ad44293405e43c9907e35e4577
Passara <passaraink@gmail.com>
2020-11-08 00:35:01 +0700

    edit accounts modal

---
+++ test_files/test_table_3.php
@@ -0,0 +1,53 @@
+<head>
+    <!-- Bootstrap CSS and JS-->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+
+    <!-- Bootstrap TABLE CSS and JS -->
+    <link href="https://unpkg.com/bootstrap-table@1.18.0/dist/bootstrap-table.min.css" rel="stylesheet">
+    <script src="https://unpkg.com/bootstrap-table@1.18.0/dist/bootstrap-table.min.js"></script>
+</head>
+
+<body>
+    <table
+      id="table"
+      data-toggle="table"
+      data-height="460"
+      data-click-to-select="true">
+      <thead>
+        <tr>
+          <th data-field="state" data-checkbox="true"></th>
+          <th data-field="id">Branch ID</th>
+          <th data-field="name">Branch Name</th>
+        </tr>
+      </thead>
+      <tbody>
+          <tr>
+              <td></td>
+              <td>dhe</td>
+              <td>dhd</td>
+          </tr>
+          
+          <tr>
+              <td></td>
+              <td>hghg</td>
+              <td>dhd</td>
+          </tr>
+          
+          <tr>
+              <td></td>
+              <td>tjv</td>
+              <td>reyf</td>
+          </tr>
+      </tbody>
+    </table>
+</body>
