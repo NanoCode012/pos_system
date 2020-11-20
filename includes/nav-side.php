57257fde953190a812fe474f1e9cc5cee55fa1c2
NanoCode012 <kevinvong@rocketmail.com>
2020-11-20 12:09:09 +0700

    Remove href on logo

--- includes/nav-side.php
+++ includes/nav-side.php
@@ -4,7 +4,7 @@
     <div class="scrollbar-inner">
         <!-- Brand -->
         <div class="sidenav-header  align-items-center">
-            <a class="navbar-brand" href="javascript:void(0)">
+            <a class="navbar-brand">
                 <img src="assets/img/brand/chanchanlogo.png" class="navbar-brand-img" alt="...">
             </a>
         </div>
